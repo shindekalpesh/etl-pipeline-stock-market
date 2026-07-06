ae7b1d99ba3be5b501024b855a348cbdad6902ca
Kalpesh Shinde <kalpeshtheofficial@gmail.com>
2026-07-06 21:44:31 +0530

    Add: extract() method. done till fetching json data from url.

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 
+import requests
+
 class ETLStockMarket:
     """
     Extracts, Transforms and Loads the stock market data of a given company.
@@ -11,3 +13,20 @@ class ETLStockMarket:
     def __str__(self):
         return f"company_name: {self.company_name}"
         
+    def extract(self):
+        load_dotenv()
+        API_KEY = os.getenv('API_KEY')
+
+        print("self.company_name", type(self.company_name), self.company_name)
+
+        url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={self.company_name}&apikey={API_KEY}"
+        print("url", type(url), url)
+
+        response = requests.get(url)
+        print("response", type(response), response)
+
+        data_json = response.json()
+        print("data_json", type(data_json), data_json)
+
+       
+
